3f89e8a4708a6d388ca1179eb8cb32902cc630b2
AnhQuan29112004 <namcaodidong2004@gmail.com>
2026-05-28 15:36:42 +0700

    feat: Add ProductFilterValues interface for enhanced product filtering

--- fe-demo/src/types/ProductType.types.tsx
+++ fe-demo/src/types/ProductType.types.tsx
@@ -5,3 +5,10 @@ export interface Product {
   quantity: number;
   image: string;
 }
+
+export interface PtoductFilterValues{
+  cap_kho?:string;
+  trang_thai?:string;
+  don_vi_quan_ly?:string;
+  can_bo_quan_ly?:string
+}
